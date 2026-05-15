de0e57247a8e80d2b190a662d154a7e59684b81f
Aryan <aryan@Aryans-MacBook-Air.local>
2026-05-15 22:46:17 +0530

    reset onboarding for fresh testing

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -59,7 +59,7 @@ export const useAppStore = create<AppState>()(
       setStreak: (days) => set({ streakDays: days }),
 
       completeOnboarding: () => {
-        set({ hasCompletedOnboarding: false, });
+        set({ hasCompletedOnboarding: false});
       },
 
       checkAndUpdateLockStatus: () => {
